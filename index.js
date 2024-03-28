1cf9c4e1002eabbb6e9492fc2520e8765a55d4ee
Dana <donotorlando@gmail.com>
2024-03-28 12:13:05 +0700

    try to change readme

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 2
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
-
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
@@ -23,6 +22,14 @@ app.get('/', function (req, res) {
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+//
+app.get('/api/whoami', function(req, res){
+  let ipAddress = req.ip;
+  const lang = req.headers['accept-language']
+  const software = req.headers['user-agent']
+  res.json({"ipaddress": `${ipAddress}`, "language" : `${lang}`, "software" : `${software}`})
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
